a3a17f9ce10b9154a940e9a772774894f595aa4e
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2023-12-26 10:50:26 +0330

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { CiSearch } from 'react-icons/ci';
+
+function App() {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=city&appid=a228caf6361612f8e1c1649e0f8de136`;
+
+  return <></>;
+}
+
+export default App;

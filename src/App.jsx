9a68fe6a2060097cbfd8a7743fc2f723be8e4447
Matin Hedayat <mhmdmatinhedayat@gmail.com>
2023-12-27 12:39:20 +0330

    Some update on logic and style

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,41 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { CiSearch } from 'react-icons/ci';
 import { Rings } from 'react-loader-spinner';
+import { FiDelete } from 'react-icons/fi';
 
 export default function App() {
   const [weather, setWeather] = useState({});
   const [location, setLocation] = useState('Tehran');
-  const [unit, setUnit] = useState('');
+  const [queryUnit, setQueryUnit] = useState('metric');
+  const [unit, setUnit] = useState('°C');
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
+  console.log(weather)
+
+  const handleChangeUnit = () => {
+    queryUnit === 'metric' ? setQueryUnit('imperial') : setQueryUnit('metric');
+  };
 
   const detailValues = [
-    { title: 'Feels Like', value: weather.main?.feels_like, unit: '°C' },
+    { title: 'Feels Like', value: weather.main?.feels_like, unit: unit },
     { title: 'Humidity', value: weather.main?.humidity, unit: '%' },
-    { title: 'Min Temp', value: weather.main?.temp_min, unit: '°C' },
-    { title: 'Max Temp', value: weather.main?.temp_max, unit: '°C' },
-    { title: 'Wind Speed', value: weather.wind?.speed, unit: 'M/S' },
+    { title: 'Min Temp', value: weather.main?.temp_min, unit: unit },
+    { title: 'Max Temp', value: weather.main?.temp_max, unit: unit },
+    {
+      title: 'Wind Speed',
+      value: weather.wind?.speed,
+      unit: queryUnit === 'metric' ? 'M/S' : 'MPH',
+    },
     { title: 'Pressure', value: weather.main?.pressure, unit: 'HPA' },
   ];
 
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a228caf6361612f8e1c1649e0f8de136${unit}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=a228caf6361612f8e1c1649e0f8de136`;
 
-  const fetchWeather = async (url) => {
+  const fetchWeather = async (url, queryUnit) => {
     setIsLoading(true);
     setError(false);
     try {
-      const res = await axios(url);
+      const res = await axios(`${url}&units=${queryUnit}`);
       setWeather(res.data);
     } catch (error) {
       setError(true);
@@ -35,13 +46,14 @@ export default function App() {
   };
 
   useEffect(() => {
-    fetchWeather(apiUrl);
-  }, []);
+    fetchWeather(apiUrl, queryUnit);
+    queryUnit === 'metric' ? setUnit('°C') : setUnit('°F');
+  }, [queryUnit]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!location) return;
-    fetchWeather(apiUrl);
+    fetchWeather(apiUrl, queryUnit);
   };
 
   return (
@@ -54,9 +66,20 @@ export default function App() {
 
       <div className='max-w-3xl h-screen relative mx-auto'>
         <form
-          className='w-4/5 absolute top-20 inset-x-1/2 -translate-x-1/2 flex backdrop-blur-sm overflow-hidden border-2 border-white/20 rounded-3xl'
+          className='w-4/5 absolute top-16 inset-x-1/2 -translate-x-1/2 flex justify-between backdrop-blur-sm overflow-hidden border-2 border-white/20 rounded-3xl'
           onSubmit={handleSubmit}
         >
+          <button
+            className='absolute w-max h-max inset-y-1/2 -translate-y-1/2 right-[24%] text-white/30 text-xl'
+            type='button'
+            onClick={() => {
+              setLocation('');
+              setError(true);
+            }}
+          >
+            <FiDelete />
+          </button>
+
           <input
             className='bg-transparent w-4/5 text-white caret-white placeholder-white/50 px-6 py-4 tracking-widest outline-none'
             type='text'
@@ -82,24 +105,31 @@ export default function App() {
           </p>
         ) : (
           <>
-            <div className='w-4/5 absolute top-52 inset-x-1/2 -translate-x-1/2 text-white'>
+            <div className='w-4/5 absolute top-44 inset-x-1/2 -translate-x-1/2 text-white'>
               <div>
                 <span>{weather.name}</span>
                 <span className='bg-white/10 text-xs px-3 py-1 ml-2 backdrop-blur-md rounded-lg'>
                   {weather.sys?.country}
                 </span>
               </div>
-              <h2 className='text-7xl font-bold mt-2 mb-6'>
-                {weather.main?.temp.toFixed()}°C
+              <h2
+                className='text-7xl font-bold mt-2'
+                onClick={handleChangeUnit}
+              >
+                {weather.main?.temp.toFixed()}
+                {unit}
               </h2>
-              <p className='absolute top-5 -right-2 text-lg font-medium -rotate-90'>
+              <p className='absolute top-3 -right-8 flex items-center text-lg font-medium -rotate-90'>
                 {weather.weather ? weather.weather[0]?.main : ''}
+                <img
+                  className='w-16 h-16 ml-2'
+                  src={`https://openweathermap.org/img/wn/${
+                    weather.weather ? weather.weather[0]?.icon : ''
+                  }@2x.png`}
+                  alt=''
+                />
               </p>
-              <p className='bg-white/20 text-sm text-white w-max px-4 py-2 backdrop-blur-md rounded-xl'>
-                {new Date().toDateString(weather.timezone)} &nbsp;{' '}
-                {new Date().toTimeString(weather.timezone).slice(0, 9)}
-              </p>
-              <div className='flex flex-wrap items-center justify-between gap-2 mt-6'>
+              <div className='flex flex-wrap items-center justify-between gap-2 mt-8'>
                 <div className='text-sm flex items-center gap-2'>
                   <p>Longitude :</p>
                   <p className='bg-white/10 px-2 py-1 backdrop-blur-sm rounded-lg'>
